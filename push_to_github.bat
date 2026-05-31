5a1142e9987b2909e30df3020baf7eac81fbeb63
ShutterGallery Admin <admin@shuttergallery.local>
2026-05-31 13:29:48 +0300

    UI: Fotograf detay sayfasina tiklanabilir profil baglantisi ve kullanici avatari eklendi

--- push_to_github.bat
+++ push_to_github.bat
@@ -1,8 +1,5 @@
 @echo off
-"C:\Program Files\Git\cmd\git.exe" init
 "C:\Program Files\Git\cmd\git.exe" add .
-"C:\Program Files\Git\cmd\git.exe" commit -m "Frontend: Add user unique IDs, pagination, 404/500 error pages and deployment configs"
-"C:\Program Files\Git\cmd\git.exe" branch -M main
-"C:\Program Files\Git\cmd\git.exe" remote add origin https://github.com/ardap00/ShutterGallery.git
-"C:\Program Files\Git\cmd\git.exe" push -u origin main
+"C:\Program Files\Git\cmd\git.exe" commit -m "UI: Fotoğraf detay sayfasına tıklanabilir profil bağlantısı ve kullanıcı avatarı eklendi"
+"C:\Program Files\Git\cmd\git.exe" push
 pause
